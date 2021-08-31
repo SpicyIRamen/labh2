15e42bd0b8648790fb4510a2f6f45d944802ebfd
Sean <sean.werner@iths.se>
2021-08-31 23:58:20 +0200

    Lab2 almost finished, search function missing

--- src/main/java/com/iths/labh2/controller/MainCharController.java
+++ src/main/java/com/iths/labh2/controller/MainCharController.java
@@ -7,7 +7,6 @@ import java.util.Optional;
 
 import javax.validation.Valid;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
